9c2ca65fa8a97881d00fb574f22cd488e4a31eff
Oluwaseye Ayooluwa Philip <depaytez@gmail.com>
2025-08-26 21:20:34 +0100

    task 3

--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { MovieCardProps } from '@/interfaces';
-// interface MovieCardProps {
-//  title: string;
-//  posterImage: string;
-//  releaseYear: string;
-// }
+import Image from "next/image";
 
 const MovieCard: React.FC<MovieCardProps> = ({title, posterImage, releaseYear}) => {
  return(
-  <div className="container">
+  <div className="h-[563px] container">
+   <div>
+    <Image 
+     className="h-[430px] w-full rounded-md hover:cursor-pointer"
+     src={posterImage} 
+     alt={title} 
+     width={100} 
+     height={100} 
+    />
+
+   </div>
    <h2>{title}</h2>
-   <img src={posterImage} alt={title} />
    <p>Release Year: {releaseYear}</p>
   </div>
  );
